e820b7101f5808125a9ac837aa3fa6de1deddf29
Anurag Singh <anurag26official@gmail.com>
2025-06-03 12:32:47 +0530

    Typescript syntax and type giving explicitly

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+// in js :-
+//  let x = 2;
+//  x = "Anurag";
+// in Ts :-
+//  let x : number = 2;
+// function greet( x : string ){
+//     console.log("Hello ${x}");
+// }
+// greet("Anurag");
+function sum(x, y) {
+    // console.log(x + y);  
+    return x + y;
+}
+;
+let ans = sum(2, 4);
+console.log(ans);
---
+++ index.ts
@@ -0,0 +1,26 @@
+// in js :-
+//  let x = 2;
+//  x = "Anurag";
+
+// in Ts :-
+//  let x : number = 2;
+
+
+
+// function greet( x : string ){
+//     console.log("Hello ${x}");
+// }
+
+// greet("Anurag");
+
+
+function sum(x:number,y:number): number{
+    // console.log(x + y);  
+    return x + y;
+};
+
+let ans : number = sum(2,4);
+
+console.log(ans);
+
+
