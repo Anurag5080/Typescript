ef08a05f22eb5b1d3ea927e08e86330d298feb30
Anurag Singh <anurag26official@gmail.com>
2025-06-21 15:17:27 +0530

    Create TS-setup.ts
    Setup guide for TS in Nodejs project

---
+++ TS-setup.ts
@@ -0,0 +1,4 @@
+// How to setup Typescript with your project:-
+
+// 1. npm install --dev typescript (ideally it should be a dev dependency.) (You can also install it globally.)
+// 2. npx tsc --init (makes tsconfig.json that tells the TS compiler about a lots of jargons.)
