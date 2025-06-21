5bb1dea3c59dfc882442ec908c8b60cac8cb1606
Anurag Singh <anurag26official@gmail.com>
2025-06-21 15:41:58 +0530

    Update TS-setup.ts

--- TS-setup.ts
+++ TS-setup.ts
@@ -2,3 +2,4 @@
 
 // 1. npm install --dev typescript (ideally it should be a dev dependency.) (You can also install it globally.)
 // 2. npx tsc --init (makes tsconfig.json that tells the TS compiler about a lots of jargons.)
+// 3. npx tsc -b (To convert TS code to JS before running your application.)
